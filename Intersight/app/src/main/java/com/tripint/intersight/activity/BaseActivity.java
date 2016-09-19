78a2d83ab8b21b1838532007a03c20d1db30b086
lirichen <lirichen@Richard-iMac.local>
2016-09-19 11:03:07 +0800

    Update for AskAnswerFragment layout

---
+++ Intersight/app/src/main/java/com/tripint/intersight/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.tripint.intersight.activity;
+
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by lirichen on 16/9/13.
+ */
+public class BaseActivity extends ActivityCompat {
+}
--- Intersight/app/src/main/java/com/tripint/intersight/fragment/AskAnswerFragment.java
+++ Intersight/app/src/main/java/com/tripint/intersight/fragment/AskAnswerFragment.java
@@ -4,44 +4,60 @@ package com.tripint.intersight.fragment;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.tripint.intersight.R;
 import com.tripint.intersight.adapter.AskAnswerPageAdapter;
 import com.tripint.intersight.adapter.MyTabPagerAdapter;
+import com.tripint.intersight.common.utils.ToastUtil;
 import com.tripint.intersight.model.QAModel;
 import com.tripint.intersight.common.widget.recyclerviewadapter.BaseQuickAdapter;
 import com.tripint.intersight.common.widget.recyclerviewadapter.listener.OnItemChildClickListener;
+import com.tripint.intersight.widget.BannerViewHolder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AskAnswerFragment extends Fragment {
 
-    @Bind(R.id.ask_tabLayout)
-    TabLayout mTabLayout;
-
     @Bind(R.id.recycler_view_ask_answer)
     RecyclerView mRecyclerView;
 
-    private List<Fragment> fragmentList;
-    private TabLayout.Tab tabOne,tabTwo,tabThree;//标签栏目
+    @Bind(R.id.btn_qa_profession)
+    TextView mQAPrefessionButton;
 
-    private List<String> stringList;
-    private MyTabPagerAdapter myTabPagerAdapter;
+    @Bind(R.id.btn_qa_interest)
+    TextView mQAInterestrButton;
+
+    @Bind(R.id.btn_qa_recommend)
+    TextView mQARecommendButton;
+
 
     private AskAnswerPageAdapter mAdapter;
+
+    private List<String> networkImages;
+
+    private String[] images = {
+            "http://img2.imgtn.bdimg.com/it/u=3093785514,1341050958&fm=21&gp=0.jpg",
+            "http://img2.3lian.com/2014/f2/37/d/40.jpg",
+            "http://d.3987.com/sqmy_131219/001.jpg"};
 
     public AskAnswerFragment() {
         // Required empty public constructor
@@ -57,38 +73,72 @@ public class AskAnswerFragment extends Fragment {
 
         initView(view);
         initAdapter();
+        setTab(0);//默认选中第一个TAB
         return view;
     }
 
     private void initView(View view) {
-//        viewPager = ((ViewPager) view.findViewById(R.id.ask_viewPager));
 
-        stringList = new ArrayList<String>();
-        stringList.add("行业领域");
-        stringList.add("我的收藏");
-        stringList.add("精选推荐");
-
-        tabOne = mTabLayout.newTab();
-        tabTwo = mTabLayout.newTab();
-        tabThree = mTabLayout.newTab();
-
-        tabOne.setText(stringList.get(0));
-        tabOne.setText(stringList.get(1));
-        tabOne.setText(stringList.get(2));
-
-        mTabLayout.addTab(tabOne);
-        mTabLayout.addTab(tabTwo);
-        mTabLayout.addTab(tabThree);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        //网络加载
+        networkImages= Arrays.asList(images);
 
-//        viewPager.setAdapter(myTabPagerAdapter);
-
-//        tabLayout.setupWithViewPager(viewPager);
 
 
     }
+
+    private View getHeaderView(View.OnClickListener clickListener){
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.view_ask_answer_banner_header, null);
+        view.setLayoutParams(new DrawerLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        ConvenientBanner mainBanner = (ConvenientBanner)view.findViewById(R.id.banner_qa_main);
+        mainBanner.setPages(new CBViewHolderCreator<BannerViewHolder>() {
+            @Override
+            public BannerViewHolder createHolder() {
+                return new BannerViewHolder();
+            }
+        }, networkImages)
+                //小圆点
+                .setPageIndicator(new int[]{R.mipmap.ic_page_indicator, R.mipmap.ic_page_indicator_focused});
+        mainBanner.setOnClickListener(clickListener);
+        return view;
+    }
+
+    @OnClick({R.id.btn_qa_recommend, R.id.btn_qa_interest, R.id.btn_qa_profession})
+    public void onTabBarClick(View view){
+        switch (view.getId()) {
+
+            case R.id.btn_qa_profession: //行业领域
+                if (!mQAPrefessionButton.isSelected()) {
+                    setTab(0);
+                }
+                break;
+            case R.id.btn_qa_interest: //我的关注
+                if (!mQAInterestrButton.isSelected()) {
+                    setTab(1);
+                }
+                break;
+            case R.id.btn_qa_recommend: //精选推荐
+                if (!mQARecommendButton.isSelected()) {
+                    setTab(2);
+                }
+                break;
+        }
+    }
+
+    /**
+     * 请求不同的关键字 精选自由行、省心国内游、品质出境游
+     *
+     * @param tab
+     */
+    private void setTab(int tab) {
+        mQAPrefessionButton.setSelected(tab == 0);
+        mQAInterestrButton.setSelected(tab == 1);
+        mQARecommendButton.setSelected(tab == 2);
+//        httpLocationRelationShips(RequestParameters.homePssociate[tab]);
+    }
+
 
     private void initAdapter() {
         mAdapter = new AskAnswerPageAdapter();
@@ -109,7 +159,17 @@ public class AskAnswerFragment extends Fragment {
                 Toast.makeText(getActivity(), content, Toast.LENGTH_LONG).show();
             }
         });
+
+        mAdapter.addHeaderView(getHeaderView(getHeaderViewClickListener()));
         mRecyclerView.setAdapter(mAdapter);
     }
 
+    private View.OnClickListener getHeaderViewClickListener(){
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ToastUtil.showToast(getActivity(), "Header view Click");
+            }
+        };
+    }
 }
