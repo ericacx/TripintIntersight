c277e72d4a4108311cbdecc745a145f5a5ca4115
ericacx <ericacx>
2016-09-21 18:48:48 +0800

    个人主页,我的页面UI

---
+++ Intersight/app/src/main/java/com/tripint/intersight/fragment/HisOpinionFragment.java
@@ -0,0 +1,31 @@
+package com.tripint.intersight.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tripint.intersight.R;
+
+/**
+ * 他的观点----页面
+ * A simple {@link Fragment} subclass.
+ */
+public class HisOpinionFragment extends Fragment {
+
+
+    public HisOpinionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_his_opinion, container, false);
+    }
+
+}
