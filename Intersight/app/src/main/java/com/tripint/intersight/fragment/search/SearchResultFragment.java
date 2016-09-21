779805f5ff31c544ba017aa7c2ca0ac279cd0025
lirichen <lirichen@Richard-iMac.local>
2016-09-21 21:16:13 +0800

    Update for SearchResult Fragment

--- Intersight/app/src/main/java/com/tripint/intersight/fragment/search/SearchResultFragment.java
+++ Intersight/app/src/main/java/com/tripint/intersight/fragment/search/SearchResultFragment.java
@@ -6,10 +6,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tripint.intersight.R;
+import com.tripint.intersight.adapter.SearchDropMenuAdapter;
+import com.tripint.intersight.common.widget.filter.DropDownMenu;
+import com.tripint.intersight.common.widget.filter.interfaces.OnFilterDoneListener;
 import com.tripint.intersight.fragment.base.BaseBackFragment;
+import com.tripint.intersight.model.search.FilterUrl;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,7 +23,7 @@ import butterknife.ButterKnife;
  * Created by lirichen on 2016/9/21.
  */
 
-public class SearchResultFragment extends BaseBackFragment {
+public class SearchResultFragment extends BaseBackFragment implements OnFilterDoneListener {
 
     public static final String ARG_SEARCH_TYPE = "arg_search_type";
     public static final String ARG_SEARCH_KEYWORD = "arg_search_keyword";
@@ -32,6 +37,12 @@ public class SearchResultFragment extends BaseBackFragment {
     TextView toolbarSearchText;
     @Bind(R.id.toolbar)
     Toolbar toolbar;
+    @Bind(R.id.toolbar_search_button)
+    ImageView toolbarSearchButton;
+    @Bind(R.id.mFilterContentView)
+    TextView mFilterContentView;
+    @Bind(R.id.search_drop_down_menu)
+    DropDownMenu searchDropDownMenu;
 
     private int searchType; //1:人 ;  2:内容
 
@@ -65,11 +76,27 @@ public class SearchResultFragment extends BaseBackFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_search_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_search_result, container, false);
         ButterKnife.bind(this, view);
 
         initView(view);
+        initFilterDropDownView();
         return view;
+    }
+
+    private void initFilterDropDownView() {
+        String[] titleList = new String[]{"第一个", "第二个", "第三个", "第四个"};
+        searchDropDownMenu.setMenuAdapter(new SearchDropMenuAdapter(mActivity, titleList, this));
+    }
+
+    @Override
+    public void onFilterDone(int position, String positionTitle, String urlValue) {
+        if (position != 3) {
+            searchDropDownMenu.setPositionIndicatorText(FilterUrl.instance().position, FilterUrl.instance().positionTitle);
+        }
+
+        searchDropDownMenu.close();
+        mFilterContentView.setText(FilterUrl.instance().toString());
     }
 
     private void initView(View view) {
