76f77a604900f9ba01ce51cb6e448729b2c0118a
LiRiChen <lirichen@163.com>
2016-09-24 11:05:10 +0800

    Update Add WX Login and share sdk

--- Intersight/app/src/main/java/com/tripint/intersight/activity/LoginActivity.java
+++ Intersight/app/src/main/java/com/tripint/intersight/activity/LoginActivity.java
@@ -8,16 +8,8 @@ import com.tripint.intersight.R;
 import com.tripint.intersight.activity.base.BaseActivity;
 import com.tripint.intersight.common.fragmentation.anim.DefaultVerticalAnimator;
 import com.tripint.intersight.common.fragmentation.anim.FragmentAnimator;
-import com.tripint.intersight.event.ShareLoginEvent;
-import com.tripint.intersight.helper.CommonUtils;
-import com.umeng.socialize.UMAuthListener;
+import com.tripint.intersight.fragment.LoginFragment;
 import com.umeng.socialize.UMShareAPI;
-import com.umeng.socialize.bean.SHARE_MEDIA;
-
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-
-import java.util.Map;
 
 public class LoginActivity extends BaseActivity {
 
@@ -31,58 +23,9 @@ public class LoginActivity extends BaseActivity {
         if (savedInstanceState == null) {
             loadRootFragment(R.id.main_container, LoginFragment.newInstance());
         }
-        EventBus.getDefault().register(this);
-//        mShareAPI.deleteOauth(this, SHARE_MEDIA.WEIXIN , new UMAuthListener() {
-//            @Override
-//            public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
-//                CommonUtils.showToast("Authorize succeed");
-//            }
-//
-//            @Override
-//            public void onError(SHARE_MEDIA platform, int action, Throwable t) {
-//                CommonUtils.showToast("Authorize failed");
-//            }
-//
-//            @Override
-//            public void onCancel(SHARE_MEDIA platform, int action) {
-//                CommonUtils.showToast("Authorize cancel");
-//            }
-//
-//        });
-
-//        sharedLogin(SHARE_MEDIA.WEIXIN);
 
     }
 
-
-    @Subscribe
-    public void sharedLogin(ShareLoginEvent event) {
-
-        sharedLogin(event.platform);
-    }
-
-
-    protected void sharedLogin(SHARE_MEDIA platform) {
-
-        mShareAPI.doOauthVerify(this, platform, new UMAuthListener() {
-            @Override
-            public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
-                CommonUtils.showToast("Authorize succeed");
-            }
-
-            @Override
-            public void onError(SHARE_MEDIA platform, int action, Throwable t) {
-                CommonUtils.showToast("Authorize failed");
-            }
-
-            @Override
-            public void onCancel(SHARE_MEDIA platform, int action) {
-                CommonUtils.showToast("Authorize cancel");
-            }
-
-        });
-
-    }
 
 
     @Override
