1533184faa7bd44c679a2131163c572490c52d60
lirichen <lirichen@Richard-iMac.local>
2016-09-26 14:25:16 +0800

    Update for header bugs

--- Intersight/data/src/main/java/com/tripint/intersight/interceptor/HeaderInterceptor.java
+++ Intersight/data/src/main/java/com/tripint/intersight/interceptor/HeaderInterceptor.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.tripint.intersight.common.cache.ACache;
 import com.tripint.intersight.common.enumkey.EnumKey;
+import com.tripint.intersight.common.utils.StringUtils;
 
 import java.io.IOException;
 
@@ -22,17 +23,20 @@ public class HeaderInterceptor implements Interceptor {
         this.context = context;
     }
 
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            Request original = chain.request();
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request original = chain.request();
+        String token = ACache.get(context).getAsString(EnumKey.User.USER_TOKEN);
+        if (StringUtils.isEmpty(token)) {
+            token = "";
+        }
+        Request request = original.newBuilder()
+                .header("Token", token)
+                .header("Client", "App/V1")
+                .method(original.method(), original.body())
+                .build();
 
-            Request request = original.newBuilder()
-                    .header("Token", ACache.get(context).getAsString(EnumKey.User.USER_TOKEN))
-                    .header("Client", "App/V1")
-                    .method(original.method(), original.body())
-                    .build();
-
-            return chain.proceed(request);
+        return chain.proceed(request);
 
     }
 }
